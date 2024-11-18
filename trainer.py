519ab6395f7ea102cf0123af04023995e8abf5d0
lukasz <machops@o2.pl>
2024-11-18 20:41:23 +0100

    save_top_checkpoints

--- trainer.py
+++ trainer.py
@@ -1,14 +1,40 @@
+import os
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 
 class Trainer:
-    def __init__(self, model, criterion, optimizer, device):
+    def __init__(self, model, criterion, optimizer, device, checkpoints_path="checkpoints"):
         self.model = model
         self.criterion = criterion
         self.optimizer = optimizer
         self.device = device
+        self.checkpoints_path = checkpoints_path
+        self.top_losses = []
+
+    def save_top_checkpoints(self, epoch, loss):
+        if not os.path.exists(self.checkpoints_path):
+            os.makedirs(self.checkpoints_path)
+
+        if len(self.top_losses) < 5 or loss < max(self.top_losses, key=lambda x: x[0])[0]:
+            if len(self.top_losses) == 5:
+                self.top_losses = sorted(self.top_losses, key=lambda x: x[0])
+                worst_loss, worst_epoch = self.top_losses.pop()
+                worst_path = f"{self.checkpoints_path}/best_epoch_{worst_epoch}_loss_{worst_loss:.4f}.pt"
+                if os.path.exists(worst_path):
+                    os.remove(worst_path)
+
+            self.top_losses.append((loss, epoch))
+            checkpoint_path = f"{self.checkpoints_path}/best_epoch_{epoch}_loss_{loss:.4f}.pt"
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': self.model.state_dict(),
+                'optimizer_state_dict': self.optimizer.state_dict(),
+                'loss': loss
+            }, checkpoint_path)
+            print(f"Checkpoint saved for epoch {epoch} with loss {loss:.4f}")
 
     def train_one_epoch(self, dataloader):
         self.model.train()
@@ -41,6 +67,8 @@ class Trainer:
             print(f"Epoch {epoch + 1}/{epochs}")
             train_loss = self.train_one_epoch(train_loader)
             val_loss = self.validate(val_loader)
+
+            self.save_top_checkpoints(epoch + 1, val_loss)
             print(f"Train Loss: {train_loss:.4f} | Validation Loss: {val_loss:.4f}")
 
 
